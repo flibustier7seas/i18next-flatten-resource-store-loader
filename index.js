a13966ef02ee0eb12e09ff0fa53ac4fce0181c9a
Дик Максим Евгеньевич <m.dik@2gis.ru>
2016-12-30 10:52:50 +0700

    init

---
+++ index.js
@@ -0,0 +1,47 @@
+/* eslint-env node */
+
+var fs = require("fs");
+var path = require("path");
+var flatten = require("flat");
+
+module.exports = function () {
+    var baseDirectory = path.dirname(this.resource);
+
+    var languageDefiningDirectories = fs.readdirSync(baseDirectory).filter(function (file) {
+        return fs.statSync(path.join(baseDirectory, file)).isDirectory();
+    });
+
+    var translations = {};
+
+    languageDefiningDirectories.forEach(function (directory) {
+        translations[directory] = getTranslationsFromDirectory(path.join(baseDirectory, directory));
+    });
+
+    var bundle = Object.keys(translations).reduce((messages, locale) => {
+        messages[locale] = flatten(translations[locale]);
+        return messages;
+    }, {});
+
+    return "module.exports = " + JSON.stringify(bundle);
+};
+
+function getTranslationsFromDirectory(directoryPath) {
+    var result = {};
+
+    var childrens = fs.readdirSync(directoryPath);
+
+    childrens.forEach(function (children) {
+        var childrenPath = path.join(directoryPath, children);
+
+        if (fs.statSync(childrenPath).isFile()) {
+            var extName = path.extname(childrenPath);
+            var baseName = path.basename(childrenPath, extName);
+
+            result[baseName] = JSON.parse(fs.readFileSync(childrenPath));
+        } else {
+            result[children] = getTranslationsFromDirectory(childrenPath);
+        }
+    });
+
+    return result;
+}
